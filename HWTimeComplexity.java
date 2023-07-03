61762eba7d76b74618b8d9c28531bcc778266297
MariaMarz <126455312+MariaMarz@users.noreply.github.com>
2023-07-03 21:08:14 +0200

    Add files via upload

---
+++ HWTimeComplexity.java
@@ -0,0 +1,59 @@
+package Algorithmus;
+
+public class HWTimeComplexity {
+
+
+    // 1 Задача
+
+//    START
+//    READ number n
+//    IF n ==1THEN return
+//    FOR i = 1; i<=n; i+1
+//    FOR j = 1; j<=n; j+1
+//    print "*"
+//    BREAK
+//            END
+
+            // Временная сложность составляет O(1)
+
+    // Задача 2
+//    START
+//    READ number n
+//    numbers i = 0, j = 0, a = 0
+//    FOR i = n/2; i<=n; i+1
+//    FOR j = 2; j<=n;j*2
+//    a = a + n/2
+//    END
+
+    // Временная сложность составляет O(n log n)
+
+
+
+    // Задача 3
+
+//    START
+//    READ number n
+//    number  a = 0
+//    FOR i = 0; i<n; i+1
+//    FOR j = n; j>i;j-1
+//    a = a + i + j
+//            END
+
+    // Временная сложность составляет O(n^2)
+
+
+    // Задача 4
+
+//    START
+//    READ number n
+//    numbers  a = 0, i = n
+//    WHILE i > 0
+//    a = a +i
+//            i = i/2
+//        END
+
+    // Временная сложность составляет O(log n)
+
+}
+
+
